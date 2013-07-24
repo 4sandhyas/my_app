9ab0d1566a9ffdfff6167d8eed9fdf4647036db2
sandhya <4sandhyas@gmail.com>
2013-07-24 11:37:25 +0530

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+  def index
+    @post=Post.all
+  end
+  def new
+    @post=Post.new
+  end
+  def create
+    @post=Post.new(params[:post].permit(:title, :message))
+    if @post.save
+    redirect_to @post
+    else
+      render 'new'
+    end
+    
+  end
+  def show
+    @post=Post.find(params[:id])
+    
+  end
+  def edit
+    @post=Post.find(params[:id])
+  end
+  def update
+    @post=Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :message))
+      redirect_to @post
+    else
+      render 'edit'    
+    end
+  end
+  def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+end
